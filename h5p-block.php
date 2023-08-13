68dc4d2bd4cac68fc20d97bcb65034571ae3778f
Tim Lindgren <timothy.lindgren@bc.edu>
2023-08-13 00:08:16 -0400

    troubleshooting updates

--- h5p-block.php
+++ h5p-block.php
@@ -46,12 +46,9 @@ require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $myUpdateChecker = PucFactory::buildUpdateChecker(
-	'https://github.com/cdil-bc/h5p-block/',
+	'https://github.com/cdil-bc/h5p-block',
 	__FILE__,
 	'unique-plugin-or-theme-slug'
 );
-
-//Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('main');
 
 $myUpdateChecker->getVcsApi()->enableReleaseAssets();
