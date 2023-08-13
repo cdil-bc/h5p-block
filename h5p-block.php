20616ee161c44dc017760b137e3a9ba31c2d23aa
Tim Lindgren <timothy.lindgren@bc.edu>
2023-08-13 00:03:24 -0400

    Fixing issue with plugin function

--- h5p-block.php
+++ h5p-block.php
@@ -4,7 +4,7 @@
  * Description:       Block for embedding H5P.com content
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.2
+ * Version:           0.1.3
  * Author:            Tim Lindgren
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
@@ -40,11 +40,18 @@ add_action('enqueue_block_assets', 'enqueue_h5p_resizer');
 
 // Plugn Update Checker
 // https://github.com/YahnisElsts/plugin-update-checker/
+// Plugn Update Checker
+// https://github.com/YahnisElsts/plugin-update-checker/
 require 'plugin-update-checker/plugin-update-checker.php';
 use YahnisElsts\PluginUpdateChecker\v5\PucFactory;
 
 $myUpdateChecker = PucFactory::buildUpdateChecker(
-	'https://example.com/path/to/details.json',
-	__FILE__, //Full path to the main plugin file or functions.php.
+	'https://github.com/cdil-bc/h5p-block/',
+	__FILE__,
 	'unique-plugin-or-theme-slug'
 );
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+$myUpdateChecker->getVcsApi()->enableReleaseAssets();
