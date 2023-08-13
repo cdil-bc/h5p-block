7eac664a1b2991f48d081da615b9e8f5444bba1b
Tim Lindgren <timothy.lindgren@bc.edu>
2023-08-12 23:33:52 -0400

    Testing new release

--- h5p-block.php
+++ h5p-block.php
@@ -4,7 +4,7 @@
  * Description:       Block for embedding H5P.com content
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           0.1.2
  * Author:            Tim Lindgren
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
