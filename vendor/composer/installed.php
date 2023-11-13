1abec9e8401ea95c4692d9b92075fbe07c690b94
Tim Lindgren <contact@timlindgren.com>
2023-11-13 16:20:03 -0500

    Adding composer.json

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'h5pblock/h5p-block',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'a9829febf0a7e29f9060ab6dd086f42f5c63ab8b',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'h5pblock/h5p-block' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'a9829febf0a7e29f9060ab6dd086f42f5c63ab8b',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
